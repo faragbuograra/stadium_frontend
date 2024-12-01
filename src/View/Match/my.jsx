bb757eb29c68dbf89f0f0dec3551a403ae6f57a6
faragbuograra <farag_2516@limu.edu.ly>
2024-12-01 14:15:50 +0200

    .

--- src/View/Match/my.jsx
+++ src/View/Match/my.jsx
@@ -10,7 +10,7 @@ function MYMatch(){
   const [page, setPage] = React.useState(1);
 const [data, setData] = React.useState([]);
 const [totalPages, setTotalPages] = React.useState(0);
-  const [pageSize, setPageSize] = React.useState(10);
+  const [pageSize, setPageSize] = React.useState(100);
   const [state, setState] = React.useState({});
   const [flitter, setFlitter] = React.useState("");
   const [type, setType] = React.useState("");
@@ -44,7 +44,7 @@ const [totalPages, setTotalPages] = React.useState(0);
  
   return (
     <>
-      <div style={{   margin: "20px" }}>
+      <div style={{   margin: "20px" }} dir="rtl">
      
        
        
@@ -52,50 +52,20 @@ const [totalPages, setTotalPages] = React.useState(0);
         <div class="relative flex flex-col w-full h-full text-slate-700 bg-white shadow-md rounded-xl bg-clip-border">
   <div class="relative mx-4 mt-4 overflow-hidden text-slate-700 bg-white rounded-none bg-clip-border">
     <div class="flex items-center justify-between ">
-        <div>
-            <h3 class="text-lg font-semibold text-slate-800">match List</h3>
-            <p class="text-slate-500">Review each match  before edit</p>
-        </div>
-      <div class="flex flex-col gap-2 shrink-0 sm:flex-row">
-        <button
-          class="rounded border border-slate-300 py-2.5 px-3 text-center text-xs font-semibold text-slate-600 transition-all hover:opacity-75 focus:ring focus:ring-slate-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-          type="button"
-          onClick={() => {
-            setPageSize(100)
-          }
-          }
-          >
-          View All
-        </button>
-        <button
-        onClick={
-            () => {
-                setShowadd(true);
-            }
-        }
-          class="flex select-none items-center gap-2 rounded bg-slate-800 py-2.5 px-4 text-xs font-semibold text-white shadow-md shadow-slate-900/10 transition-all hover:shadow-lg hover:shadow-slate-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-          type="button">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"
-            stroke-width="2" class="w-4 h-4">
-            <path
-              d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z">
-            </path>
-          </svg>
-          Add Match
-        </button>
-      </div>
+    
+     
     </div>
    
   </div>
   <div class="p-0 ">
-    <table class="w-full mt-4 text-left table-auto min-w-max">
+    <table class="w-full mt-4i text-center table-auto min-w-max">
       <thead>
         <tr>
           <th
             class="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-50 hover:bg-slate-100">
             <p
               class="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none text-slate-500">
-             ID
+             رقم المبارة
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
                 stroke="currentColor" aria-hidden="true" class="w-4 h-4">
                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -107,7 +77,7 @@ const [totalPages, setTotalPages] = React.useState(0);
             class="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-50 hover:bg-slate-100">
             <p
               class="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none text-slate-500">
-              stadium
+              الملعب
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
                 stroke="currentColor" aria-hidden="true" class="w-4 h-4">
                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -119,7 +89,7 @@ const [totalPages, setTotalPages] = React.useState(0);
             class="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-50 hover:bg-slate-100">
             <p
               class="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none text-slate-500">
-              player name
+             اسم حجز المبارة
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
                 stroke="currentColor" aria-hidden="true" class="w-4 h-4">
                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -131,7 +101,7 @@ const [totalPages, setTotalPages] = React.useState(0);
             class="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-50 hover:bg-slate-100">
             <p
               class="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none text-slate-500">
-         day
+       اليوم
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
                 stroke="currentColor" aria-hidden="true" class="w-4 h-4">
                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -143,7 +113,7 @@ const [totalPages, setTotalPages] = React.useState(0);
             class="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-50 hover:bg-slate-100">
             <p
               class="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none text-slate-500">
-         time
+         الوقت
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
                 stroke="currentColor" aria-hidden="true" class="w-4 h-4">
                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -155,7 +125,7 @@ const [totalPages, setTotalPages] = React.useState(0);
             class="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-50 hover:bg-slate-100">
             <p
               class="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none text-slate-500">
-         status
+       الحاله
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
                 stroke="currentColor" aria-hidden="true" class="w-4 h-4">
                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -176,22 +146,19 @@ const [totalPages, setTotalPages] = React.useState(0);
             data?.map((item, index) => (
         <tr>
           <td class="p-3 border-b border-slate-200">
-            <div class="flex items-center gap-3">
+            <div class="flex items-center w-full  text-center gap-3">
               {/* <img src="https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg"
                 alt="John Michael" class="relative inline-block h-9 w-9 !rounded-full object-cover object-center" /> */}
-              <div class="flex flex-col">
-                <p class="text-sm font-semibold text-slate-700">
+             
+                <p class="text-sm font-semibold w-full text-slate-700">
                     {item.id}   
                 </p>
-                {/* <p
-                  class="text-sm text-slate-500">
-                  john@creative-tim.com
-                </p> */}
-              </div>
+               
+              
             </div>
           </td>
           <td class="p-3 border-b border-slate-200">
-            <div class="flex flex-col">
+            <div class="flex flex-col items-center w-full">
               <p class="text-sm font-semibold text-slate-700">
                 {item.stadium.name}
               </p>
@@ -225,13 +192,19 @@ const [totalPages, setTotalPages] = React.useState(0);
           <td class="p-3 border-b border-slate-200">
             <div class="flex flex-col">
               <p class="text-sm font-semibold text-slate-700">
-                {item.status ? 'active' : 'inactive'}
+                {
+                  //if day > now 
+                  //  تم الحجز
+                  // else
+                  //  لم يتم الحجز
+                  new Date(item.day) < new Date() ? "تم اللعب" : "لم يتم اللعب"
+                }
               </p>
              
             </div>
           </td>
           <td class="p-3 border-b border-slate-200">
-            <button
+        {new Date(item.day) > new Date() ?    <button
                onClick={
                 () => {
                     setShow(true);
@@ -243,10 +216,10 @@ const [totalPages, setTotalPages] = React.useState(0);
               type="button">
               <span class="">
              <h1>
-          requst player
+         طلب اللاعبين
               </h1>
               </span>
-            </button>
+            </button> : null}
           </td>
         </tr>
       ))}
@@ -290,7 +263,7 @@ const [totalPages, setTotalPages] = React.useState(0);
        
        <div class="w-full max-w-sm min-w-[200px] mt-4">
         <label class="block mb-1 text-sm text-slate-700">
-        number_of_players
+       رقم اللاعبين المطلوب
         </label>
         <input
         value={dataEdit.number_of_players}
@@ -322,7 +295,7 @@ const [totalPages, setTotalPages] = React.useState(0);
                 class="w-full mx-auto select-none rounded border border-red-600 py-2 px-4 text-center text-sm font-semibold text-red-600 transition-all hover:bg-red-600 hover:text-white hover:shadow-md hover:shadow-red-600/20 active:bg-red-700 active:text-white active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                 type="button"
                 data-dialog-close="true">
-                Cancel
+                الغاء
             </button>
  
             <button
@@ -349,16 +322,10 @@ const [totalPages, setTotalPages] = React.useState(0);
                 class="w-full mx-auto select-none rounded bg-slate-800 py-2 px-4 text-center text-sm font-semibold text-white shadow-md shadow-slate-900/10 transition-all hover:shadow-lg hover:shadow-slate-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                 type="button"
                 data-dialog-close="true">
-                Save
+                حفظ
             </button>
         </div>
-        <p class="flex justify-center mt-4 font-sans text-sm text-slate-500">
-          Looking for more details? Contact
-          <a href="#admin"
-            class="ml-1 text-sm font-bold leading-normal text-slate-500">
-            Admin.
-          </a>
-        </p>
+      
       </div>
     </div>
   </div>   }
